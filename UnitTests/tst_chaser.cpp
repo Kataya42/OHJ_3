87954b06ac660942366352a0035d32bb7fce46ae
Ilkka Suutarinen <suutarin@linux-desktop11.tuni.fi>
2020-11-27 22:45:40 +0200

    Added documentation and accelerationTest for ChaserTest

--- UnitTests/tst_chaser.cpp
+++ UnitTests/tst_chaser.cpp
@@ -11,9 +11,22 @@ public:
     ChaserTest();
 
 private slots:
+    /**
+     * @brief sameLocTest Tests whether chaser will move even if target is on top of it.
+     */
     void sameLocTest();
+    /**
+     * @brief basicChaseTest Basic test for chasing a target along one axis
+     */
     void basicChaseTest();
+    /**
+     * @brief longerChaseTest More complicated test for chasing a target a longer distance along two axis.
+     */
     void longerChaseTest();
+    /**
+     * @brief accelerationTest Tests whether acceleration works correctly
+     */
+    void accelerationTest();
 };
 
 ChaserTest::ChaserTest()
@@ -68,6 +81,32 @@ void ChaserTest::longerChaseTest()
     }
     QVERIFY(testchaser.isClose(target, 15));
 
+}
+
+void ChaserTest::accelerationTest()
+{
+    //first test whether chaser reaches target in correct time
+    Interface::Location loc1;
+    loc1.setXY(0, 0);
+    Chaser testchaser;
+    std::shared_ptr<Chaser> target = nullptr;
+    target = std::make_shared<Chaser>();
+    target->move(loc1);
+    loc1.setXY(0, 10);
+    testchaser.move(loc1);
+    testchaser.setAcceleration(1);
+    testchaser.setMaxSpeed(10);
+    for (int i = 0; i < 4; i++) {
+        testchaser.chase(target);
+    }
+    QVERIFY(testchaser.giveLocation().giveY()== 0);
+
+    // next test if chaser decelerates and accelerates in the opposite direciton correctly.
+    target->move(loc1);
+    for (int i = 0; i < 7; i++) {
+        testchaser.chase(target);
+    }
+    QVERIFY(testchaser.giveLocation().giveY()== 0);
 }
 
 QTEST_APPLESS_MAIN(ChaserTest)
